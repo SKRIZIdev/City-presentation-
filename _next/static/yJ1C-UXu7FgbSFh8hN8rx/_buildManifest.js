c2f9e03fa2e0b1fab9ee64a255aa43c342aa9307
SKRlZI <kladienko1982@gmail.com>
2026-05-20 14:33:33 +0300

    Updates

--- _next/static/yJ1C-UXu7FgbSFh8hN8rx/_buildManifest.js
+++ _next/static/yJ1C-UXu7FgbSFh8hN8rx/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/City-presentsiton-//_next/:path+",
+        "destination": "/City-presentsiton-/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
